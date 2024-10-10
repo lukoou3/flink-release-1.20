c5b5e5b66d89d15a43f07832ccdf56bad9c1face
lifengchao5 <lifengchao5@jd.com>
2024-10-10 23:49:03 +0800

    使用java11

--- flink-examples/flink-examples-my/src/main/java/com/test/stream/CliFrontendRunTest.java
+++ flink-examples/flink-examples-my/src/main/java/com/test/stream/CliFrontendRunTest.java
@@ -5,6 +5,7 @@ import org.apache.flink.client.cli.CliFrontend;
 /**
  * 参数：
  *   run -t yarn-per-job -d -Djobmanager.memory.process.size=1g -Dtaskmanager.memory.process.size=1g -Dtaskmanager.memory.managed.size=0m -Dyarn.provided.lib.dirs=/lfc/flinklib-1.20 -p 2 -c com.test.stream.SimpleEtlTest D:\WorkSpace\flink-release-1.20\flink-examples\flink-examples-my\target\flink-examples-my-1.20-SNAPSHOT.jar aa bb
+ *   run -t yarn-per-job -d -Djobmanager.memory.process.size=1g -Dtaskmanager.memory.process.size=1g -Dtaskmanager.memory.managed.size=0m -Dyarn.provided.lib.dirs=/lfc/flinklib-1.20 -p 2 -c com.test.stream.SimpleEtlTest D:\IdeaWorkspace\flink-release-1.20\flink-examples\flink-examples-my\target\flink-examples-my-1.20-SNAPSHOT.jar aa bb
  *
  * 环境变量：
  *   FLINK_CONF_DIR=D:\java-apps\flink-1.20.0\conf
